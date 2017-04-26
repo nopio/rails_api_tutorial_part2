805a74e87a00cc4665516363e59a9f50c7bf04d8
Piotr Jaworski <piotrek.jaw@gmail.com>
2017-04-26 23:19:44 +0200

    Small refactor

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,17 +1,13 @@
 require 'rails_helper'
 
 describe Author do
-  let(:author) { create(:author) }
+  subject { create(:author) }
 
   describe 'associations' do
-    subject { author }
-
     it { should have_many(:books) }
   end
 
   describe 'validations' do
-    subject { author }
-
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
   end
